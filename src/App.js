20e323fbb9284be3b9bfde2a9acbbba55b8a4fd8
Sérgio Pedro <sergiopedro@MBP-de-Sergio.lan>
2021-02-16 16:05:08 +0000

    adds reactevents first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            passwordConfirmation: '',
+            email: '',
+            errors: []
+        };
+
+
+    }
+
+
+    submitForm(event) {
+        console.log("Submitting the form now...");
+    }
+
+    displayForm() {
+        return (
+            <div>
+                Username: <input type="text" /><br />
+                Password: <input type="text" /><br />
+                Password Confirmation: <input type="text" /><br />
+                Email: <input type="text" /><br />
+                <br />
+                <button onClick="{this.submitForm}">Submit</button>
+            </div>
+        );
+    }
+
+    render(){
+        return (
+            <div className="App">
+                Create Account
+                <hr />
+                {this.displayForm()}
+            </div>
+
+        );
+
+    }
+
+}
+submitForm(event) {
+    console.log("Submitting the form now...");
 }
 
 export default App;
